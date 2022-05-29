924af8ba7bfe6a545c7dd322b937c914d3f503a7
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-05-29 14:08:44 -0500

    updating repository structure

--- 2022/03.singleton/API.java
+++
@@ -1,5 +0,0 @@
-public class API {
-    public String getResponse() {
-        return DataBase.getDataBase().getResponse() + " of API";
-    }
-}
--- 2022/03.singleton/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DataBase dataBase = DataBase.getDataBase();
-        System.out.println(dataBase.getResponse());
-
-        API api = new API();
-        System.out.println(api.getResponse());
-    }
-}
