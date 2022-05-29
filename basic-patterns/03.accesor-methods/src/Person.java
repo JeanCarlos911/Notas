98502ae612d13ad2907677e7ac2e67455fa91d3a
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-05-29 13:56:58 -0500

    adding accesor methods pattern

---
+++ basic-patterns/03.accesor-methods/src/Person.java
@@ -0,0 +1,30 @@
+public class enum Person {
+    // always private atributtes and only edited by the accesors methods
+    private int age;
+    private boolean adult;
+
+    // getter of a no boolean atribute
+    public int getAge() {
+        return age;
+    }
+
+    // setter
+    public void setAge(int age) {
+        if(age < 0) {
+            this.age = 0;
+        }
+        else {
+            this.age = age;
+        }
+    }
+
+    // getter of a boolean
+    public isAdult() {
+        return adult;
+    }
+
+    // setter
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+}
