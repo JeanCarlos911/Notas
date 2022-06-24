6115a8a9ad5a60b4993f901a9161d8fad7480aa1
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-06-23 22:36:46 -0500

    adding figuras dibujables

---
+++ 00.university-repo/2022/04.figuras/src/Ventana.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+class Ventana extends JFrame{
+    private JButton btPoligono;
+    private JButton btCirculo;
+    private JScrollPane scroll;
+
+    public Ventana(){
+        crearBotones();
+        crearPanel();
+
+        setSize(1000,700);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void crearPanel(){
+        scroll = new JScrollPane();
+        scroll.setBounds(32, 96, 500, 500);
+        add(scroll);
+    }
+
+    public void crearBotones() {
+        btPoligono = new JButton("Dibujar Poligono");
+        btPoligono.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 14));
+        btPoligono.setBounds(32, 32, 172, 23);
+        add(btPoligono);
+
+        btCirculo = new JButton("Dibujar circulo");
+        btCirculo.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 14));
+        btCirculo.setBounds(224, 32, 172, 23);
+        btCirculo.addActionListener(e -> dibujarCirculo());
+        add(btCirculo);
+    }
+
+    public void dibujarCirculo() {
+        Circulo circulo = new Circulo(200);
+        Dibujable dibujo = new CirculoDibujable(circulo);
+        scroll.removeAll();
+        scroll.add(dibujo);
+        scroll.repaint();
+    }
+}
